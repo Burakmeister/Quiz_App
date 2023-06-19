145d3d8004f5c5d1b27e1565f3df5a740334b37b
Grzechosz <grzechosz125@gmail.com>
2023-06-19 22:41:58 +0200

    added some popups

--- AddNewQuiz.xaml.cs
+++ AddNewQuiz.xaml.cs
@@ -1,18 +1,8 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Quiz_App
 {
@@ -103,6 +93,16 @@ namespace Quiz_App
             InvalidQuestionInputPopup.IsOpen = true;
             await Task.Delay(2000);
             InvalidQuestionInputPopup.IsOpen = false;
+        }
+
+        private void showCloseQuizCreationPopup(object sender, RoutedEventArgs e)
+        {
+            CloseQuizCreationPopup.IsOpen = true;
+        }
+
+        private void hideCloseQuizCreationPopup(object sender, RoutedEventArgs e)
+        {
+            CloseQuizCreationPopup.IsOpen = false;
         }
     }
 }
--- Homepage.xaml.cs
+++ Homepage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using Org.BouncyCastle.Asn1.BC;
+﻿using Quiz_App.DAOs;
+using Quiz_App.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Quiz_App.Models;
-using Quiz_App.DAOs;
 
 namespace Quiz_App
 {
@@ -25,42 +13,45 @@ namespace Quiz_App
     public partial class Homepage : Page
     {
         public List<Quiz> Quizzes { get; set; }
+        public List<Result> Results { get; set; }
 
-        //public class Quiz
-        //{
-        //    public string Name { get; set; }
-        //    public string Date { get; set; }
+        public class Result
+        {
+            public string Name { get; set; }
+            public string Date { get; set; }
 
-        //    public Quiz()
-        //    {
-        //        Name = String.Empty;
-        //        Date = String.Empty;
-        //    }
-        //}
+            public Result()
+            {
+                Name = String.Empty;
+                Date = String.Empty;
+            }
+        }
 
         public Homepage() //nie jestem pewien ale wydaję mi się że trzeba rebuildować ten widok przy każdej zmianie czyli by użytkownik zobaczył zaktualizowany widok należy ... new Homepage();
         {
             InitializeComponent();
             QuizDao quizDao = new QuizDao();
             Quizzes = (List<Quiz>?)quizDao.findAll();
-            //    new List<Quiz>
-            //{
-            //    new Quiz { Name = "Quiz 1", Date = "01.01.2021" },
-            //    new Quiz { Name = "Test 2", Date = "02.02.2021" },
-            //    new Quiz { Name = "Test 3", Date = "03.03.2021" },
-            //    new Quiz { Name = "Quiz 1", Date = "04.01.2021" },
-            //    new Quiz { Name = "Test 2", Date = "05.02.2021" },
-            //    new Quiz { Name = "Test 3", Date = "06.03.2021" },
-            //    new Quiz { Name = "Quiz 1", Date = "07.01.2021" },
-            //    new Quiz { Name = "Test 2", Date = "08.02.2021" },
-            //    new Quiz { Name = "Test 3", Date = "09.03.2021" },
-            //    new Quiz { Name = "Quiz 1", Date = "10.01.2021" },
-            //    new Quiz { Name = "Test 2", Date = "11.02.2021" },
-            //    new Quiz { Name = "Test 3", Date = "12.03.2021" },
-            //    new Quiz { Name = "Quiz 1", Date = "13.01.2021" },
-            //    new Quiz { Name = "Test 2", Date = "14.02.2021" },
-            //    new Quiz { Name = "Test 3", Date = "15.03.2021" },
-            //};
+
+            Results = new List<Result>
+            {
+                new Result { Name = "Quiz 1", Date = "01.01.2021" },
+                new Result { Name = "Quiz 2", Date = "01.01.2021" },
+                new Result { Name = "Quiz 3", Date = "01.01.2021" },
+                new Result { Name = "Quiz 4", Date = "01.01.2021" },
+                new Result { Name = "Quiz 5", Date = "01.01.2021" },
+                new Result { Name = "Quiz 6", Date = "01.01.2021" },
+                new Result { Name = "Quiz 7", Date = "01.01.2021" },
+                new Result { Name = "Quiz 8", Date = "01.01.2021" },
+                new Result { Name = "Quiz 9", Date = "01.01.2021" },
+                new Result { Name = "Quiz 10", Date = "01.01.2021" },
+                new Result { Name = "Quiz 11", Date = "01.01.2021" },
+                new Result { Name = "Quiz 12", Date = "01.01.2021" },
+                new Result { Name = "Quiz 13", Date = "01.01.2021" },
+                new Result { Name = "Quiz 14", Date = "01.01.2021" },
+                new Result { Name = "Quiz 15", Date = "01.01.2021" },
+                new Result { Name = "Quiz 16", Date = "01.01.2021" },
+            };
             DataContext = this;
         }
 
@@ -127,6 +118,16 @@ namespace Quiz_App
             {
                 mainWindow.NavigateToTakeQuiz();
             }
+        }
+
+        private void showQuizResultsPopup(object sender, RoutedEventArgs e)
+        {
+            QuizResults.IsOpen = true;
+        }
+
+        private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            // to do obsługa zmiany wyboru w liście wyników quizu
         }
     }
 }
